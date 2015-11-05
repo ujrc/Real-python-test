de20cee201b1b451354a4eb228bb2b44d6d6bf56
ujrc <uwjearc@yahoo.com>
2015-11-04 20:25:24 -0600

    fabfile

--- flasktaskr_6/fabfile.py
+++ flasktaskr_6/fabfile.py
@@ -1,22 +1,5 @@
 from fabric.api import local, settings,abort
 from fabric.contrib.console import confirm
-
-"""def test():
-    local("nosetests -v")
-
-def commit():
-
-    message = raw_input("Enter a git commit message: ")
-    local("git add . && git commit -am '{}'".format(message))
-
-def push():
-    local("git push origin master")
-
-def prepare():
-    test()
-    commit()
-    push()
-"""
 def test():
     with settings(warn_only=True):
         result=local("nosetests -v", capture=True)
